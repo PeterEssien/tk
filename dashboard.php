359a03c22403114601a00cded38745ad81573760
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-09-08 11:30:38 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -422,9 +422,8 @@ include("auth.php");
 				<div class="container">
 					<div class="row text-center align-items-center flex-row-reverse">
 						<div class="col-lg-auto ml-lg-auto">
-							<ul class="list-inline list-inline-dots mb-0">
-								<li class="list-inline-item"><a href="https://github.com/l33d4n/USDT-wallet" target="_blank" class="link-secondary">Source code</a></li>
-							</ul>
+							
+							
 						</div>
 						<div class="col-12 col-lg-auto mt-3 mt-lg-0">
 							Copyright © 2023 Powerd By
