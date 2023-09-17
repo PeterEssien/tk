0b4042f582f62ea355eb2e26a3d3502d37c9252a
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-09-17 20:16:53 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -245,6 +245,19 @@ include("auth.php");
 		document.body.style.display = "block"
 
 	</script>
+		<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/650750190f2b18434fd90703/1hai91p9e';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->	
 </body>
 
 </html>
